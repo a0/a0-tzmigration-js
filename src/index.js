5a2d032e01c3536e030617cefdfce9f83aee7b18
Aldrin Martoq <a@a0.cl>
2018-05-28 18:57:59 -0400

    First working version.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,144 @@
+import axios from 'axios'
+
 class TZVersion {
-  constructor(name, version, url = "foo") {
+  constructor(base_url = "https://a0.github.com/a0-tzmigration-ruby/data/") {
+    this.base_url = base_url
+  }
+
+  convert_date(object, key) {
+    object[key] = new Date(object[key])
+  }
+
+  fetch(name, version) {
     this.name = name
     this.version = version
-    this.url = url
+    return new Promise((resolve, reject) => {
+      axios.get(this.base_url + 'timezones/' + name + '.json').then(response => {
+        this.data = response.data
+        if (!(version in response.data.versions)) {
+          throw `Version ${version} not found`
+        }
+
+        let version_data = response.data.versions[version]
+        if (version_data.alias) {
+          this.fetch(version_data.alias, version).then(resolve).catch(reject)
+        } else {
+          resolve(version_data)
+        }
+      }).catch(reject)
+    })
+  }
+
+  timestamps(version_data) {
+    let result = []
+
+    result.push(-Infinity)
+    version_data.transitions.forEach(transition => {
+      result.push(transition.utc_timestamp)
+    })
+    result.push(Infinity)
+
+    return result
+  }
+
+  timezone_ranges(version_data) {
+    let ini = -Infinity
+    let fin = Infinity
+
+    let ranges = []
+    version_data.transitions.forEach(transition => {
+      ranges.push({ ini: ini, fin: (ini = transition.utc_timestamp), off: transition.utc_prev_offset })
+    })
+
+    if (ranges.length > 0) {
+      let last = ranges[ranges.length - 1]
+      ranges.push({ ini: last.fin, fin: fin, off: version_data.transitions[version_data.transitions.length - 1].utc_offset })
+    }
+
+    return ranges
+  }
+
+  next_index(index, ranges, timestamp) {
+    while (((index + 1) < ranges.length) && (ranges[index].ini < timestamp) && (ranges[index].fin <= timestamp)) {
+      index += 1
+    }
+
+    return index
+  }
+
+  split_range_list(ranges, timestamps) {
+    let index = this.next_index(0, ranges, timestamps[0])
+
+    timestamps.forEach((timestamp, timestamp_index) => {
+      let range = ranges[index]
+
+      if (timestamp > range.ini && timestamp < range.fin && index < ranges.length) {
+        ranges.splice(index + 1, 0, Object.assign({}, range, { ini: timestamp }))
+        ranges[index].fin = timestamp
+      }
+
+      if (timestamp_index + 1 < timestamps.length) {
+        index = this.next_index(index, ranges, timestamps[timestamp_index + 1])
+      }
+    })
+
+    return ranges
+  }
+
+  compact_ranges(ranges) {
+    let index = 0
+
+    while (index < ranges.length) {
+      let curr = ranges[index]
+      let next = ranges[index + 1]
+
+      if (next && curr.fin == next.ini && curr.off == next.off) {
+        curr.fin = next.fin
+        ranges.splice(index + 1, 1)
+      } else {
+        index += 1
+      }
+    }
+
+    return ranges
+  }
+
+  delta_ranges(a, b) {
+    let timestamps_a = this.timestamps(a)
+    let timestamps_b = this.timestamps(b)
+    let timestamps = timestamps_a.concat(timestamps_b).sort( (a, b) => a - b).filter((item, index, array) => {
+      return index == array.length -1 || array[index+ 1] != item
+    })
+
+    let list_a = this.split_range_list(this.timezone_ranges(a), timestamps)
+    let list_b = this.split_range_list(this.timezone_ranges(b), timestamps)
+
+    let delta = []
+    list_a.forEach((range_a, index) => {
+      let range_b = list_b[index]
+
+      if (range_a.off != range_b.off) {
+        delta.push({ ini: range_a.ini, fin: range_a.fin, off: range_b.off - range_a.off })
+      }
+    })
+
+    return this.compact_ranges(delta)
+  }
+
+  timezones() {
+    return new Promise((resolve, reject) => {
+      axios.get(this.base_url + 'timezones/00-index.json').then(response => {
+        resolve(response.data.timezones)
+      }).catch(reject)
+    })
+  }
+
+  versions() {
+    return new Promise((resolve, reject) => {
+      axios.get(this.base_url + 'versions/00-index.json').then(response => {
+        resolve(response.data.versions)
+      }).catch(reject)
+    })
   }
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,84 @@
+/**
+ * @jest-environment node
+ */
 import { TZVersion } from '../src/index'
 
-test('something', () => {
-  let a = new TZVersion('America/Santiago', '2016a')
-  expect(a.version).toBe('2016a')
+test('timezones', async () => {
+  const timezones = await new TZVersion().timezones()
+  expect(Object.keys(timezones)).toEqual(expect.arrayContaining(['America/Santiago', 'Zulu']))
+  expect(timezones['America/Santiago'].versions).toEqual(expect.arrayContaining(['2018c', '2018e']))
+})
+
+test('versions', async () => {
+  const versions = await new TZVersion().versions()
+  expect(Object.keys(versions)).toEqual(expect.arrayContaining(['2013c', '2018e']))
+  expect(versions['2013c'].timezones).toEqual(expect.arrayContaining(['America/Santiago', 'Zulu']))
+})
+
+test('fetch timezone version', async () => {
+  const data = await new TZVersion().fetch('America/Santiago', '2018e')
+  expect(data.released_at).toEqual('2018-05-01 23:42:51 -0700')
+})
+
+test('fetch aliased timezone version', async () => {
+  let tzversion = new TZVersion()
+  const data = await tzversion.fetch('America/Caracas', '2018e')
+  expect(data.released_at).toEqual('2018-05-01 23:42:51 -0700')
+})
+
+test('returns an empty list for America/Santiago between 2014i 2014j versions', async () => {
+  let tzversion = new TZVersion()
+  const data_a = await tzversion.fetch('America/Santiago', '2014i')
+  const data_b = await tzversion.fetch('America/Santiago', '2014j')
+  let delta = tzversion.delta_ranges(data_a, data_b)
+  expect(delta.length).toBe(0)
+})
+
+test('returns a non empty list for America/Santiago between 2014i 2015a versions', async () => {
+  let tzversion = new TZVersion()
+  const data_a = await tzversion.fetch('America/Santiago', '2014i')
+  const data_b = await tzversion.fetch('America/Santiago', '2015a')
+  let delta = tzversion.delta_ranges(data_a, data_b)
+  expect(delta.length).not.toBe(0)
+})
+
+test('returns the delta range expected for America/Caracas', async () => {
+  let tzversion = new TZVersion()
+  const data_a = await tzversion.fetch('America/Caracas', '2016c')
+  const data_b = await tzversion.fetch('America/Caracas', '2016d')
+  let delta = tzversion.delta_ranges(data_a, data_b)
+
+  expect(delta.length).toBe(1)
+  expect(delta[0].off).toBe(1800)
+  expect(delta[0].ini).toBe(Date.parse('2016-05-01T02:30:00-04:30') / 1000)
+})
+
+
+let compare_inverse = async (zone_a, version_a, zone_b, version_b) => {
+  let tzversion = new TZVersion()
+  const data_a = await tzversion.fetch(zone_a, version_a)
+  const data_b = await tzversion.fetch(zone_b, version_b)
+  let delta_ab = tzversion.delta_ranges(data_a, data_b)
+  let delta_ba = tzversion.delta_ranges(data_b, data_a)
+
+  expect(delta_ab.length).toBe(delta_ba.length)
+
+  delta_ab.forEach((item, index) => {
+    let item_a = delta_ab[index]
+    let item_b = delta_ba[index]
+
+    expect(item_a.ini).toBe(item_b.ini)
+    expect(item_a.fin).toBe(item_b.fin)
+    expect(item_a.off).toBe(-item_b.off)
+  });
+}
+
+let f = (a, b) => [].concat(...a.map(a => b.map(b => [].concat(a, b))))
+let cartesian = (a, b, ...c) => b ? cartesian(f(a, b), ...c) : a
+
+let versions = ['2013c', '2015a', '2016a', '2018e']
+cartesian(versions, versions).forEach(([version_a, version_b]) => {
+  test('returns the inverse delta range list for America/Santiago between 2013c 2018e versions', () => {
+    compare_inverse('America/Santiago', version_a, 'America/Santiago', version_b)
+  })
 })
